76b9fcb97f49ce18cdd4a85e6924e218c1fb5db8
orange <759550141@qq.com>
2018-04-10 18:28:53 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,6 +1,24 @@
 // Write your cade below:
-function main() {};
-
+function main(Numbers,Guess_Numbers) {
+  let num_arr=Numbers.split('');
+  let guess_arr=Guess_Numbers.split('');
+  let result={'A':0,'B':0};
+  let str_result='';
+  for(let i=0; i<num_arr.length;i++){
+    if(num_arr[i]==guess_arr[i]) result.A++;
+    else {
+      if(include_num(guess_arr[i],num_arr)) result.B++;
+    }
+  }
+  str_result=`${result.A}A${result.B}B`;
+  return str_result;
+};
+function include_num(number,sta_arr){
+  for(let num of sta_arr){
+    if(number==num) return true;
+  }
+  return false;
+}
 module.exports = {
     main
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,25 @@
-const main = require('../main/main');
-
-describe('main()', () => {
-
+const load = require('../main/main');
+const main=load.main;
+var Numbers='1234'
+describe('main()', function() {
+  it('4个数字和位置都猜对',function(){
+    let result=main(Numbers,'1234');
+    expect(result).toEqual('4A0B');
+  })
+  it('数字都对，部分位置不对',function(){
+    let result=main(Numbers,'1342');
+    expect(result).toEqual('1A3B');
+  })
+  it('部分数字不对，位置都对',function(){
+    let result=main(Numbers,'1536');
+    expect(result).toEqual('2A0B');
+  })
+  it('部分数字不对，部分位置不对',function(){
+    let result=main(Numbers,'1532');
+    expect(result).toEqual('2A1B');
+  })
+  it('4个数字不对',function(){
+    let result=main(Numbers,'7896');
+    expect(result).toEqual('0A0B');
+  })
 });
